c56d8f0444a6f959369816c29156c922637a3e74
unknown <cogjava911@iiht.tech>
2022-06-19 17:28:23 +0530

    initial commit

---
+++ src/test/java/com/auction/portal/ojhaauctiondiscoveryservice/OjhaAuctionDiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.portal.ojhaauctiondiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjhaAuctionDiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
